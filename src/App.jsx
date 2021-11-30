f496ea47b644d367f499241f506b4ea4143662be
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-11-30 18:50:28 +0100

    initial configuration

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Feedback from './components/feedback/Feedback';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Feedback />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/feedback/Feedback.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import styles from './Feedback.module.css';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Feedback
